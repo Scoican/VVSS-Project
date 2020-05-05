bb9a5106b324f884f1bfcc3138bbb12c01e7bdc9
Scoican Ovidiu <scoican.ovidiu@gmail.com>
2020-05-05 19:31:59 +0300

    Last Commit

---
+++ PizzaShop/src/test/java/pizzashop/repository/PaymentRepositoryTestMockito.java
@@ -0,0 +1,38 @@
+package pizzashop.repository;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import pizzashop.model.Payment;
+import pizzashop.model.PaymentType;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+
+class PaymentRepositoryTestMockito {
+
+    private String filePath = "C:\\Users\\Scoican\\Desktop\\Work\\Validation and verification of software systems\\VVSS-Project\\PizzaShop\\src\\main\\resources\\data\\paymentsTest.txt";
+
+
+    @Test
+    void testAddValidPayment() {
+        Payment payment = new Payment(1, PaymentType.CARD, 10);
+        PaymentRepository paymentRepository = new PaymentRepository(filePath);
+        try {
+            paymentRepository.add(payment);
+            assertTrue(true);
+        } catch (Exception e) {
+            fail();
+        }
+    }
+
+    @Test
+    void testAddNullPayment() {
+        PaymentRepository paymentRepository = new PaymentRepository(filePath);
+        try {
+            paymentRepository.add(null);
+            fail();
+        } catch (Exception e) {
+            assertTrue(true);
+        }
+    }
+}
